26313feee3f0aafa6319711474d7c110d4654132
SandhiyaPS <sandhiya.ps@optisolbusiness.com>
2023-06-19 17:57:48 +0530

    new changes

---
+++ components/index.ts
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import Footer from "./Footer";
+import NavBar from "./Navbar";
+import CarCard from "./CarCard";
+import ShowMore from "./ShowMore";
+import SearchBar from "./SearchBar";
+import CustomFilter from "./CustomFilter";
+import CustomButton from "./CustomButton";
+
+export {
+  Hero,
+  NavBar,
+  Footer,
+  CarCard,
+  ShowMore,
+  SearchBar,
+  CustomFilter,
+  CustomButton,
+}
